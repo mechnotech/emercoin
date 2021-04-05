c90aa65ae27cedb5bff8956b5a84072176bb733f
Evgenii Shumilov <mechnotech@yandex.ru>
2021-04-05 11:26:01 +0300

    Страницы ошибок 404 и 500

--- emercoin/urls.py
+++ emercoin/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
 urlpatterns += i18n_patterns(
     path('', include('info.urls')),
     path('documentation/', include('emerdocs.urls'))
-    #prefix_default_language=False
+    # prefix_default_language=False
 )
 
 if settings.DEBUG:
@@ -22,3 +22,6 @@ if settings.DEBUG:
                           document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
+
+handler404 = "info.views.page_not_found"
+handler500 = "info.views.server_error"
